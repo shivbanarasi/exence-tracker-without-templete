ce302ef0e705a45b83128953d0dcf272a3316ec9
shivbanarasi <shivbanarasi0542@gmail.com>
2023-03-26 12:19:50 +0530

    login signup complete

--- route/signup.js
+++ route/signup.js
@@ -28,25 +28,41 @@ route.get('/login',(req,res)=>{
 route.post('/login',async(req,res)=>{
     const email=req.body.email;
     const password=req.body.password;
-    db.query(`select password from user where email = '${email}'`,(err,data)=>{
-            console.log(data[0].password);
-            if(data[0].password===password){
-                res.render('home',{
-                    title:'home page'
-                });
+    db.query('select email from user',async(err,data)=>{
+        const userfind=()=>{
+            for(let i of data){
+                console.log(i.email);
+                if(email===i.email){
+                    return true
+                }
             }
-            else{
-                res.render('login',{
-                    title:'login page',
-                    massage:'you have entered wrong password'
-                })
-            }
-            
-
-    });
-
-     
-})
+            return false;
+        }
+        console.log(userfind());
+        if(userfind()){
+            db.query(`select password from user where email = '${email}'`,async(err,data)=>{
+                const pass=await data[0].password;
+                console.log(pass);
+                if(pass===password){
+                    res.redirect('/')
+                }
+                else{
+                    res.render('login',{
+                        title:'login page',
+                        massage:'password incorrect'
+                    })
+                }
+        });
+       // res.send('true')
+        }
+        else  {
+            res.render('login',{
+                title:'login page',
+                massage:'user doesnot exist'
+            })
+        }
+      })
+    })  
 
 route.post('/signup',(req,res)=>{
     const name=req.body.name;
@@ -55,31 +71,47 @@ route.post('/signup',(req,res)=>{
     const password=req.body.password;
     const conpass=req.body.conpass;
           db.query('select email from user',(err,data)=>{
-            for(let i of data){
-                if((password===conpass)&&(email===i.email)){
-                    console.log('password matched and email is unique')
-                    db.query(`insert into user(name,email,phone,password,conpass) 
-                values('${name}','${email}',${phone},${password},${conpass})`,(err,result)=>{
-                    if(err){
-                        console.log(err)
-                    }       
-                } )
-                res.redirect('/')
-                }else{
-                    
-                        res.render('signup',{
-                            title:'signup page',
-                            massage:'password doesnot match'
-                
-                        
-                    }  )
-                } 
+            const uservalid=()=>{
+                for(let i of data){
+                    if(email===i.email){
+                        console.log('password matched and email is unique')
+                        return true;  
             }
+        }
+        return false;
+    }
+    console.log(uservalid());
+    if(uservalid()){
+        res.render('signup',{
+            title:'signup page',
+            massage:'this user already exist'
+        })
+    }
+    else{
+        if(password===conpass){
+        db.query(`insert into user(name,email,phone,password,conpass) 
+        values('${name}','${email}',${phone},${password},${conpass})`,(err,result)=>{
+            if(err){
+                console.log(err)
+            }       
+        } )
+        res.redirect('/')
+        }else{
             
-          })  
-       
-   });
-   
+                res.render('signup',{
+                    title:'signup page',
+                    massage:'password doesnot match'
+        
+                
+            }  )
+        } 
+
+
+    }
+})
+})
+            
+                   
 
 
 
