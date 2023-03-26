815cea79419bd3d88a996fc6038e495cb0349df4
shivbanarasi <shivbanarasi0542@gmail.com>
2023-03-26 15:17:34 +0530

    encrypting password

--- route/signup.js
+++ route/signup.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const bcrypt=require('bcrypt');
 const route=express.Router();
 const db=require('../mysql/connn')
 
@@ -43,19 +44,22 @@ route.post('/login',async(req,res)=>{
             db.query(`select password from user where email = '${email}'`,async(err,data)=>{
                 const pass=await data[0].password;
                 console.log(pass);
-                if(pass===password){
-                    res.redirect('/')
-                }
-                else{
+                bcrypt.compare(password, pass, function(err, result) {
+                  if(result){
+                    res.redirect('/');
+                    console.log('password matched')
+                  }
+                  else{
                     res.render('login',{
                         title:'login page',
                         massage:'password incorrect'
                     })
                 }
-        });
-       // res.send('true')
+                })
+            })
         }
-        else  {
+
+       else  {
             res.render('login',{
                 title:'login page',
                 massage:'user doesnot exist'
@@ -70,6 +74,9 @@ route.post('/signup',(req,res)=>{
     const phone=req.body.phone;
     const password=req.body.password;
     const conpass=req.body.conpass;
+    
+    console.log('this')
+    
           db.query('select email from user',(err,data)=>{
             const uservalid=()=>{
                 for(let i of data){
@@ -89,25 +96,22 @@ route.post('/signup',(req,res)=>{
     }
     else{
         if(password===conpass){
-        db.query(`insert into user(name,email,phone,password,conpass) 
-        values('${name}','${email}',${phone},${password},${conpass})`,(err,result)=>{
+            bcrypt.hash(password,10,(err,password)=>{
+                db.query(`insert into user(name,email,phone,password) 
+        values('${name}','${email}',${phone},'${password}')`,(err,result)=>{
             if(err){
                 console.log(err)
             }       
         } )
         res.redirect('/')
-        }else{
-            
-                res.render('signup',{
-                    title:'signup page',
-                    massage:'password doesnot match'
-        
-                
-            }  )
-        } 
-
-
-    }
+    })    
+}else{
+    res.render('signup',{
+        title:'signup page',
+        massage:'password doesnot match'
+})
+}
+}
 })
 })
             
