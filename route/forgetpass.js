4a9389f2f3f6fe71cb5cd6a873116445a339edc2
shivbanarasi <shivbanarasi0542@gmail.com>
2023-03-30 20:30:00 +0530

    forget

--- route/forgetpass.js
+++ route/forgetpass.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const bcrypt=require('bcrypt');
 const route=express.Router();
+const { v4: uuidv4 } = require('uuid');
 const db=require('../mysql/connn')
 const Sib=require('sib-api-v3-sdk')
-const { v4: uuidv4 } = require('uuid');
 require('dotenv').config()
 
 route.get('/forgetpass',(req,res)=>{
@@ -16,20 +16,19 @@ route.get('/forgetpass',(req,res)=>{
 })
 
 route.post('/forgetpass',(req,res,next)=>{
+    const id=uuidv4();
     const email=req.body.email
-    console.log(email)
     const client=Sib.ApiClient.instance
     const apiKey=client.authentications['api-key']
     apiKey.apiKey=process.env.API_KEY
     const transEmailApi=new Sib.TransactionalEmailsApi()
 
-    const id=uuidv4();
     db.query(`insert into forgetpass(id,userid,isactive) 
- values('${id}','${email}',"no")`,(err,result)=>{
-     if(err){
-         console.log(err)
-     }       
- } )
+    values('${id}','${email}',"no")`,(err,result)=>{
+        if(err){
+            console.log(err)
+        }       
+    } )
 
     const sender={
         email:'shivbanarasi0542@gmail.com',
@@ -45,27 +44,36 @@ route.post('/forgetpass',(req,res,next)=>{
         to: receiver,
         subject:'change your password',
         textContent:'here is your link to change your password',
-        htmlContent:`<a href="http://localhost:4000/forgetpass/${email}">link to reset password</a>`
+        htmlContent:`<a href="http://localhost:4000/forgetpass/${id}">link to reset password</a>`
     })
     .then(console.log)
     .catch(console.log)
+   
+   
     
 })
 
-route.get('/forgetpass/:email',(req,res)=>{
-   const email=req.params.email
+route.get('/forgetpass/:id',(req,res)=>{
+   const id=req.params.id
    db.query(`update forgetpass
    set isactive='yes' 
-   where userid="${email}"`,(err,result)=>{
+   where id="${email}"`,(err,result)=>{
 if(err){
    console.log(err)
-}       
+}      
 } )
+db.query(`select email from forgetpass where id=${id}`,(err,res)=>{
     res.render('conform',{
         title:"reset password",
         massage:" ",
-        email:email
+        email:res[0]
     })
+})
+    // res.render('conform',{
+    //     title:"reset password",
+    //     massage:" ",
+    //     email:email
+    // })
 })
 
 route.post('/resetpass/:email',(req,res)=>{
